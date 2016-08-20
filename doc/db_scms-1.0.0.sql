501153425a28d99746550ffd31bc9173aba9f509
ramostear <ramostear@163.com>
2016-08-20 17:31:37 +0800

    创建了t_app站点信息表

--- doc/db_scms-1.0.0.sql
+++ doc/db_scms-1.0.0.sql
@@ -40,6 +40,25 @@ CREATE TABLE `t_manager` (
 
 insert  into `t_manager`(`id`,`managername`,`managernickname`,`password`,`createtime`,`roleid`) values (2,'administrator','系统管理员','123456','2016-08-18 18:21:01',1);
 
+
+/*
+ * Table structure for table `t_app`
+ */
+DROP TABLE IF EXISTS `t_app`;
+
+CREATE TABLE `t_app` (
+   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '站点编号',
+   `appName` varchar(100) COLLATE utf8_bin NOT NULL COMMENT '站点名称',
+   `appDescription` varchar(500) COLLATE utf8_bin NOT NULL COMMENT '站点描述',
+   `appLogo` varchar(500) COLLATE utf8_bin DEFAULT NULL COMMENT '站点logo',
+   `appkeyword` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '站点关键字',
+   `appCopyright` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '版权信息',
+   `appUrl` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '站点域名',
+   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+   `appManagerId` int(11) DEFAULT NULL COMMENT '站点创建者编号',
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
