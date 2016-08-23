ec20e6e4e0a8591e2369e2d5bbc2c301dbbb6f01
ramostear <ramostear@163.com>
2016-08-23 09:25:00 +0800

    对managerService完成了基本的测试

--- src/test/java/com/seelecloud/cms/testservice/ManagerServiceTest.java
+++ src/test/java/com/seelecloud/cms/testservice/ManagerServiceTest.java
@@ -11,8 +11,8 @@
 package com.seelecloud.cms.testservice;
 
 import java.util.Date;
+import java.util.List;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -21,8 +21,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.seelecloud.cms.entity.Manager;
 import com.seelecloud.cms.service.ManagerService;
 
+import static org.junit.Assert.*;
+
 /** 
- * @Desc: () 
+ * @Desc: (管理员业务层测试) 
  * @author: 谭朝红 
  * @date: 2016年8月18日 下午5:50:32 
  * @email:ramostear@163.com 
@@ -43,18 +45,39 @@ public class ManagerServiceTest {
 	@Test
 	public void testAddManager(){
 		Manager manager = new Manager();
-		manager.setManagerName("administrator");
-		manager.setManagerNickname("超级管理员");
+		manager.setManagerName("appadmin");
+		manager.setManagerNickname("站点管理员");
 		manager.setPassword("123456");
-		manager.setRoleId(1);
+		manager.setRoleId(2);
 		manager.setCreateTime(new Date());
 		managerService.save(manager);
 	}
+	@Test
+	public void testUpdate(){
+		Manager manager = this.managerService.findById(2);
+		assertNotNull(manager);
+		manager.setManagerNickname("系统管理员");
+		this.managerService.update(manager);
+		System.out.println(manager.getManagerNickname());
+	}
+	@Test
+	public void testFindByName(){
+		Manager manger = this.managerService.findByName("administrator");
+		assertNotNull(manger);
+	}
+	@Test
+	public void testDelete(){
+		Manager manager = this.managerService.findById(4);
+		assertNotNull(manager);
+		this.managerService.delete(manager.getId());
+		Manager m = this.managerService.findById(4);
+		assertNull(m);
+	}
 	
-	@After
-	public void after(){
-		//TODO 还原数据现场
-		System.out.println("还原数据现场，保存数据库不受污染");
+	@Test
+	public void testFindAll(){
+		List<Manager> managerList = this.managerService.ListAll();
+		assertSame(1, managerList.size());
 	}
 	
 }
